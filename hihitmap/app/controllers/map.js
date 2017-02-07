d228488c54fd0c91b5fcf4e98458c4b06e648abc
Daler Asrorov <asrorids@gmail.com>
2017-02-06 22:14:07 -0800

    Fixed the column layout

--- hihitmap/app/controllers/map.js
+++ hihitmap/app/controllers/map.js
@@ -5,5 +5,11 @@ const zoomDefaultVal = 3;
 export default Ember.Controller.extend({
   lat: 45.519743,
   lng: -122.680522,
-  zoom: zoomDefaultVal
+  zoom: zoomDefaultVal,
+  actions: {
+    updateCenter(e) {
+      let center = e.target.getCenter();
+      console.log('lat', center.lat);
+    }
+  }
 });
