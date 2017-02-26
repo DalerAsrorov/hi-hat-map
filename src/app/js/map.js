87f11ac0f432be5929cd791e14b9635d98af2d4a
Daler Asrorov <asrorids@gmail.com>
2017-02-25 20:19:34 -0800

    Added maps from mapquest

--- src/app/js/map.js
+++ src/app/js/map.js
@@ -1,10 +1,26 @@
 
 // Initializing the map
-let mainMap = L.map('mapWrapper').setView([51.505, -0.09], 13);
+// let mainMap = L.map('mapWrapper').setView([51.505, -0.09], 13);
 
-L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+// L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     maxZoom: 19,
+//     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+// }).addTo(mainMap);
+
+let mapLayer = MQ.mapLayer(), mainMap;
+
+mainMap = L.map('mapWrapper', {
+    layers: mapLayer,
+    center: [ 40.731701, -73.993411 ],
+    zoom: 12
+});
+
+L.control.layers({
+  'Map': mapLayer,
+  'Hybrid': MQ.hybridLayer(),
+  'Satellite': MQ.satelliteLayer(),
+  'Dark': MQ.darkLayer(),
+  'Light': MQ.lightLayer()
 }).addTo(mainMap);
 
 export default mainMap;
