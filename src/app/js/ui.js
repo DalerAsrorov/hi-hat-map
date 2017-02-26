9dc9eb48e4741a59a73b0ed32862bfb56d8b351e
Daler Asrorov <asrorids@gmail.com>
2017-02-25 19:57:39 -0800

    Fixed issue with overflow-y

--- src/app/js/ui.js
+++ src/app/js/ui.js
@@ -3,10 +3,25 @@
 export function slideToggleCp(targetID, heightSetterID = 'arrowPointerWrapper', cpDefaultHeight = "35%", ...rest) {
     const cpNavHeight = document.getElementById(heightSetterID).offsetHeight.toString();
     const cpWrapperHeight = document.getElementById(targetID).offsetHeight.toString();
+    const slidingTime = 300;
+    const defaultTop = "-15px";
+
+    const $querySearchForm = $('#querySearchForm');
+    const $target = $(`#${targetID}`);
+    const $body = $('body');
 
     if(cpWrapperHeight === cpNavHeight) {
-       $(`#${targetID}`).animate({height: cpDefaultHeight}, 200);
-   } else {
-        $(`#${targetID}`).animate({height: cpNavHeight}, 200);
-   }
+        $body.css({"overflowY": "auto"});
+        $target.animate({height: cpDefaultHeight}, slidingTime);
+        $querySearchForm.animate({
+            top: "-15px"
+        }, slidingTime);
+    }
+    else {
+        $body.css({"overflowY": "hidden"});
+        $target.animate({height: cpNavHeight}, slidingTime);
+        $querySearchForm.animate({
+            top: "-34px"
+        }, slidingTime);
+    }
 };
