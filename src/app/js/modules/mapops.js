e48a4e25d4b41b5692689d03decd8e180a7a39f0
Daler Asrorov <asrorids@gmail.com>
2017-03-18 02:12:51 -0700

    added more stuff to work on

--- src/app/js/modules/mapops.js
+++ src/app/js/modules/mapops.js
@@ -8,8 +8,12 @@
 //     console.log("")
 // }
 
-export const generateResults = R.curry(function(data, geo) {
+export const generateResults = R.curry((data, geo) => {
     R.pipe(
         R.map(console.log), // render points with animations
     )(data);
+});
+
+export const drawPoint = R.curry((pointData) => {
+
 });
