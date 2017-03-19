f45454796138200a6a9c215d7c65eeaf0c145974
Daler Asrorov <asrorids@gmail.com>
2017-03-18 21:04:02 -0700

    created abstracted methods for map elements

--- src/app/js/modules/mapops.js
+++ src/app/js/modules/mapops.js
@@ -1,5 +1,6 @@
 import Map from './map.js';
 import { IMAGES } from './constants.js';
+import * as MapElements from './mapelements.js';
 
 // export function generateResults(data) {
 //     console.log('Data', data);
@@ -11,23 +12,13 @@ import { IMAGES } from './constants.js';
 
 export const generateResults = R.curry((data, geo) => {
     R.pipe(
-        R.map(drawPoint), // render points with animations
+        R.map(drawMarker), // render points with animations
     )(data);
 });
 
-export const drawPoint = R.curry((pointData) => {
-    // console.log('data', pointData);
-    // console.log('CONSTANTS: ', IMAGES.SOC_MEDIA_ICONS.TWITTER);
-    let twitterIcon = L.icon({
-        iconUrl: IMAGES.SOC_MEDIA_ICONS.TWITTER,
-        shadowUrl: IMAGES.SOC_MEDIA_ICONS.TWITTER,
-
-        iconSize:     [38, 95], // size of the icon
-        shadowSize:   [50, 64], // size of the shadow
-        iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-        shadowAnchor: [4, 62],  // the same for the shadow
-        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-    });
+export const drawMarker = R.curry((pointData) => {
+    const twitterIcon = MapElements.createIcon(
+                                               IMAGES.SOC_MEDIA_ICONS.TWITTER);
 
     L.marker([51.5, -0.09], {icon: twitterIcon}).addTo(Map);
 });
