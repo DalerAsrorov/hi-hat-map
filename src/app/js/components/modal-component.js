af3dca8e434e1aa046d90d0ca58b63c76789c6d4
DalerAsrorov <asrorids@gmail.com>
2017-07-19 00:21:59 -0700

    Added more methods and POCs for them

--- src/app/js/components/modal-component.js
+++ src/app/js/components/modal-component.js
@@ -1,22 +1,25 @@
 import Component from './component';
+import { isEmpty } from 'ramda';
 
 const MODAL_BASE_CLASS = 'modal fade';
-const MODAL_HEADER_DIV = '<div class="modal-header"></div>'
-const MODAL_CONTENT_DIV = '<div class="modal-content"></div>';
-
+const MODAL_HEADER_HTML = '<div class="modal-header"></div>'
+const MODAL_CONTENT_HTML = '<div class="modal-content"></div>';
+const MODAL_FOOTER_HTML = '<div class="modal-footer"></div>';
+const MODAL_CLOSE_BUTTON_HTML = '<button type="button" class="btn btn-secondary" data-dismiss="modal"></button>';
+const MODAL_CLOSE_ICON = '&times;';
 
 export default class ModalComponent extends Component {
     constructor(id, parent, nodeType, content, modelContent=null) {
         super(id, parent, nodeType, content);
-        this.$modalContent = modelContent ? modelContent : MODAL_CONTENT_DIV;
+        this.$modalContent = modelContent ? modelContent : MODAL_CONTENT_HTML;
     }
 
     buildHeader(title, handleOnCloseCallback=()=>{}) {
-        let $modalHeaderWrapper = $(MODAL_HEADER_DIV);
+        let $modalHeaderWrapper = $(MODAL_HEADER_HTML);
         let $header = $('<h3 class="modal-title"></h3>');
         let $closeButton = $(`
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&timees;</span>
+                <span aria-hidden="true">${MODAL_CLOSE_ICON}</span>
             </button>
         `);
 
@@ -26,6 +29,27 @@ export default class ModalComponent extends Component {
         $modalHeaderWrapper.append($header);
         $modalHeaderWrapper.append($closeButton);
         this.$modalContent.append($modalHeaderWrapper);
+    }
+
+    buildContent(html='') {
+        let resultContent = html;
+
+        if (!isEmpty(resultContent)) {
+            const template = '';
+        } else {
+
+        }
+
+        return resultContent;
+    }
+
+    buildFooter(customButtons=[], closeButtonText='Close') {
+        const $closeButton = $(MODAL_CLOSE_BUTTON_HTML);
+        $closeButton.append(closeButtonText);
+
+        customButtons.map((button) => {
+
+        });
     }
 
     _buildTemplate() {
