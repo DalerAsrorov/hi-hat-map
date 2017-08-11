14fda932af27bb96de1a2bdbd3e19e196814707f
Daler Asrorov <asrorids@gmail.com>
2017-08-11 02:24:39 -0700

    Yelp Fusion OAuth token is taken

---
+++ src/api/yelp/setup.js
@@ -0,0 +1,36 @@
+const send = require('@tonybadguy/call-me-maybe');
+
+const BASE = 'https://api.yelp.com';
+const OAUTH_URL = `${BASE}/oauth2/token`;
+
+const APP_PARAMS = {
+    grant_type: 'client_credentials',
+    client_id: 'TtdxZywYg33t0-nhl0zVig',
+    client_secret: 'XegowVnpX2PAX84SbXwo8m5g3q5amtkxnJfKHuElgIY7gUS1OEPR3hdemAJNW4cz'
+};
+
+const getAccessToken = () => {
+    return new Promise((res, rej) => {
+        send({
+            url: OAUTH_URL,
+            method: 'post',
+            urlencodedBody: APP_PARAMS
+        })
+            .then(response => {
+                const { statusCode, jsonBody } = response;
+
+                if (statusCode !== 200) {
+                    return rej(new Error('Response successfull, but data not available.'));
+                }
+
+                return res(jsonBody);
+            })
+            .catch(error => {
+                return rej(error);
+            });
+    });
+};
+
+module.exports = {
+    getAccessToken
+};
---
+++ src/api/yelp/yelp.js
@@ -0,0 +1,5 @@
+const setup = require('./setup');
+
+const { getAccessToken } = setup;
+
+// getAccessToken().then(data => console.log('works', data));
